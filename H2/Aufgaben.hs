a1f5706090721f8f8785b968c7f10a311131bf8f
Henri Heyden <henri@heyden5.de>
2023-10-30 17:51:23 +0100

    ... now

--- H2/Aufgaben.hs
+++ H2/Aufgaben.hs
@@ -82,14 +82,15 @@ getValue hand
 data SearchTree = ET | FT SearchTree Int SearchTree
  deriving (Show, Eq)
 
+-- TODO: UNCOMMENT
 -- If a tree is empty then just set the value and append two empty trees
 -- Ff not then decide and append to the left or right subtree
-insert :: Int -> SearchTree -> SearchTree
+-- insert :: Int -> SearchTree -> SearchTree
 -- this looks so cursed
-insert num ET = FT ET num ET
-insert num (FT left val right)
-    | num < val = FT (insert num left) val right
-    | otherwise = FT left val (insert num right)
+-- insert num ET = FT ET num ET
+-- insert num (FT left val right)
+    -- | num < val = FT (insert num left) val right
+    -- | otherwise = FT left val (insert num right)
 
 -- 2.2
 -- The empty tree should always return False since there is nothing in it
@@ -139,6 +140,114 @@ delete num (FT left val right)
     | val < num = FT left val (delete num right)
     | otherwise = FT left val right -- Return the normal tree if something went wrong (which won't)
 
--- 3.1
+
+-- TASK 3 IS READY TO BE SERVED
+-- 3.0
 -- same definition as in the searchtree
-data Tree = E | F Tree Int Tree
+-- only difference is polymorphism
+-- wow these tasks were a lot easier
+-- I propably made the last task way more harder bc I didn't use lists
+data Tree a = E | F (Tree a) a (Tree a)
+ deriving (Show, Eq)
+
+-- 3.1
+sumTree :: Tree Int -> Int
+sumTree E = 0
+sumTree (F a num b) = sumTree a + num + sumTree b
+
+-- 3.2
+mirrorTree :: Tree a -> Tree a
+mirrorTree E = E
+-- (a, val, b) maps to (b, val a)
+mirrorTree (F a val b) = F (mirrorTree b) val (mirrorTree a)
+
+-- 3.3.1
+toList :: Tree a -> [a]
+toList E = []
+toList (F a val b) = toList a ++ [val] ++ toList b
+
+-- 3.3.2
+-- the implementation has a runtime of O(n),
+-- because the recursion will break down to the case that the (sub-) tree is empty
+-- every node gets exactly one call of this method and ++ has on average constant runtime (if it's implemented like it should be)
+
+-- 4.1.1.1
+reverse1 :: [a] -> [a]
+-- IB : reverse of empty is empty
+reverse1 [] = []
+-- IS : [a, ...] should map to [reverse ... a]
+reverse1 (a : lst) = reverse1 lst ++ [a]
+
+-- 4.1.1.2
+-- the runtime is bigger than O(n) maybe O(n) * log n, but there seem to be some differences in time in each execution
+
+-- 4.1.2
+-- by temporary storing the currently calculated result,
+-- we can iteratively change it while removing elements of the list that contains the elements,
+-- that should be stacked in front of the temporary result
+-- by doing that, we only have to use the memory that the list itself would need.
+-- The big chain of recursive recalls does not have to be stored, nor accessed
+-- This results in an optimal runtime of O(n)
+reverse2 :: [a] -> [a]
+reverse2 a = fst (revAkk [] a)
+    where
+        -- maps (temp), (todo) -> (nextTemp, nextTodo)
+        revAkk :: [a] -> [a] -> ([a], [a])
+        revAkk temp [] = (temp, [])
+        revAkk temp (a : restTodo) = revAkk (a : temp) restTodo
+
+-- 4.2
+-- uses recursive iteration with recSearch
+-- can't be polymorphic, because the existance of Eq is not guaranteed
+indexOf :: Int -> [Int] -> Maybe Int
+indexOf key [] = Nothing
+indexOf key list = recSearch key list 0
+    where
+        recSearch :: Int -> [Int] -> Int -> Maybe Int
+        recSearch key [] num = Nothing
+        recSearch key (currElem : restList) num
+            | key == currElem = Just num
+            | otherwise = recSearch key restList (num + 1)
+
+-- 4.3
+-- Straightforward
+tails :: [a] -> [[a]]
+tails [] = [[]]
+tails (currElem : restList) = (currElem : restList) : tails restList
+
+-- Here you need a bit of magic
+-- The idea is that you first reverse the list and scan from the last elements similar to tails but write them the other way around.
+inits :: [a] -> [[a]]
+inits [] = [[]]
+inits (currElem : restList) = helper (reverse2 (currElem : restList))
+    where
+        helper :: [a] -> [[a]]
+        helper [] = [[]]
+        helper (currElem : restList) = helper restList ++ [restList ++ [currElem]]
+
+-- 4.4
+-- inserts an element into a defined position in a list
+insertInAt :: a -> [a] -> Int -> [a]
+insertInAt element list 0 = element : list
+-- good luck reading that
+insertInAt element (current : nextList) index = current : insertInAt element nextList (index - 1)
+
+-- iterates between all indexes in a list and executes insertInAt for each of them
+insert :: a -> [a] -> [[a]]
+insert val list = recu val list 0
+    where
+        recu :: a -> [a] -> Int -> [[a]]
+        recu elem [] num = []
+        recu elem list num
+            -- you need to stop one value after the length since the result will have |M| + 1 insertions.
+            | num == (length list + 1) = []
+            | otherwise = insertInAt elem list num : recu elem list (num + 1)
+
+-- 4.5
+-- perms :: [a] -> [[a]]
+-- perms [] = [[]]
+-- perms (elem: list) = insert elem list ++ insert elem (reverse2 list)
+
+-- subsets :: [a] -> [[a]]
+-- subsets [] = [[]]
+-- subsets (elem: list) = insert elem list ++ subsets list
