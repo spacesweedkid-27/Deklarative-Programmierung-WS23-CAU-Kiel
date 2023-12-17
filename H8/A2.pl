f52b43d0b913fcea66be4c5b5200dc684e7ebf5b
Amaroq <nike-marie@hotmail.de>
2023-12-17 13:24:00 +0100

    A2 wip

--- H8/A2.pl
+++ H8/A2.pl
@@ -6,16 +6,33 @@ person(solo).
 person(organa).
 person(mothma).
 
-% dislikes for positions
-secre(organa,mothma).
-treas(solo,skywalker).
+skywalker(Role).
+solo(Role).
+organa(Role).
+mothma(Role).
 
-% general likes
-genboard(skywalker,organa).
-genboard(solo,mothma).
-genboard(mothma,solo).
+% roles
+roles(secretary).
+roles(traesurer).
+roles(chairman).
+roles(boardmember).
 
-% likes for positions
-chair(organa,solo).
+%dislikes(solo,boardmember(mothma)).
+%dislikes(skywalker,traesurer(solo)).
+%dislikes(mothma,secretary(organa)).
+%likes(organa,boardmember(skywalker)).
+%likes(solo,chairman(organa)).
 
-board(Secretary,Traesurer,Chairman) :- secre()
+%skywalker(roles(secretary)) :- na
+%skywalker(roles(traesurer)) :- na
+%skywalker(roles(chairman)) :- na
+organa(roles(_)) :- skywalker(roles(_)).
+organa(roles(secretary)) :- \+ mothma(roles(_)).
+organa(roles(chairman)) :- solo(roles(_)).
+%organa(roles(traesurer)) :- na
+solo(roles(_)) :- \+ mothma(roles(_)), organa(roles(chairman)).
+solo(roles(traesurer)) :- skywalker(roles(_)).
+mothma(roles(_)) :- \+ solo(roles(_)).
+mothma(roles(_)) :- \+ organa(roles(secretary)).
+
+board(Person,Role) :- 
