e4b1929bcccd3ff23ac0c5ec5d4437d9addc022a
Henri Heyden <henri@heyden5.de>
2024-01-10 17:43:38 +0100

    Temp commit, there is a difference in the first change, but I will find it later.

---
+++ H10/A1.pl
@@ -0,0 +1,20 @@
+rev([]    , []) .   % reverse [] is []
+rev([X|Xs], Zs) :- app(Ys, [X], Zs),    % Search for Ys as the list that when appended the head of the input, results in the supposed reverse
+                                        % for the list [1,2,3] X would be 1 and Ys would be [3,2] since [3,2] ++ [1] == [3,2,1]
+                   rev(Xs, Ys),         % now check if the sublist Ys is really the reverse of Xs,
+                                        % which would be in our example [3,2] being reversed to [2,3]
+                   !.                   % if the two conditions are not met, rev is not proovalble, since there is no next rule.
+                                        % if the two conditions are met, find out if ' ' is provable, which is true.
+
+app([]    , Ys, Ys    ) .   % append something to empty list -> something
+app([X|Xs], Ys, [X|Zs]) :- app(Xs, Ys, Zs). % Zs is Xs appended to Ys
+
+% 2.
+% The conclusion seen in the comments about the cut operator suggest that removing it will not make a difference.
+% Now lets look at the other change:
+
+rev2([]    , []) .   % reverse [] is []
+rev2([X|Xs], Zs) :- rev2(Xs, Ys),       % Search for Ys so that Xs and Ys are reverse to each other
+                                        % This will result in cutting off X until
+                    app(Ys, [X], Zs).   % 
+                                        %
