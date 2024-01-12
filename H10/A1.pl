e2fa22c06ceb0106fa3f91de04325480d1fb5ef9
Henri Heyden <henri@heyden5.de>
2024-01-12 13:39:50 +0100

    Corrected A1

--- H10/A1.pl
+++ H10/A1.pl
@@ -3,25 +3,27 @@ rev([X|Xs], Zs) :- app(Ys, [X], Zs),    % Search for Ys as the list that when ap
                                         % for the list [1,2,3] X would be 1 and Ys would be [3,2] since [3,2] ++ [1] == [3,2,1]
                    rev(Xs, Ys),         % now check if the sublist Ys is really the reverse of Xs,
                                         % which would be in our example [3,2] being reversed to [2,3]
-                   !.                   % if the two conditions are not met, rev is not proovalble, since there is no next rule.
-                                        % if the two conditions are met, find out if ' ' is provable, which is true.
+                   !.                   % In this case, this means that if there is another rule for app(Ys, [X], Zs) (here the second rule),
+                                        % then don't try to proove the statement.
 
 app([]    , Ys, Ys    ) .   % append something to empty list -> something
 app([X|Xs], Ys, [X|Zs]) :- app(Xs, Ys, Zs). % Zs is Xs appended to Ys
 
 % 2.
-% The conclusion seen in the comments about the cut operator suggest that removing it will not make a difference.
-% for reference, this is how the cut operator is discribed in the script:
-% In Prolog kann ! anstelle von Literalen im Regelrumpf stehen:
-% p :- q, !, r.
-% Operational bedeutet dies: Wird diese Regel zum Beweis von p benutzt, dann gilt:
-% 1. Falls q nicht beweisbar ist: wähle nächste Regel für p.
-% 2. Falls q beweisbar ist: p ist nur beweisbar, falls r beweisbar ist. Mit anderen Worten,
-% es wird kein Alternativbeweis für q und keine andere Regel für p ausprobiert.
-%
+% The conclusion seen in the comments about the cut operator suggests that it will try to proove app(Ys, [X], Zs) with the second rule,
+% when it can apply the first rule (aka when the lists are short enough). As one may easily see, if the lists are short enough,
+% applying the second rule will result in another query that there is no rule to apply to.
+% This results in prolog waisting resources on branches that will not be able to be prooven.
+% This is an example of a green cut.
 % Now lets look at the other change:
 
 rev2([]    , []) .   % reverse [] is []
 rev2([X|Xs], Zs) :- rev2(Xs, Ys),       % Search for Ys so that Xs and Ys are reverse to each other
                                         % This will result in cutting off X until Xs is empty, resulting in Ys = []
                     app(Ys, [X], Zs).   % Now we append the cut off X's at Ys until we have the full list reversed.
+
+% now because we first search for reverse sublists and then append them, we don't have a problem of failing braches anymore,
+% since if rev2(Xs, Ys) fails, there is no reason to search for alternate proofs and app(Ys, [X], Zs) always will pick the correct rule,
+% we don't need a cut, EXCEPT when the sublists are that small so that there are two possible rules for app to use,
+% then it again may pick the wrong rule (the second one), resulting in some wasted resources again.
+% if one would add another cut at the end of the rule, then we would cut off these branches so fix it.
