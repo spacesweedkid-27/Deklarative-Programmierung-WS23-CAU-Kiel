4515a694db55265b34555e4f092b0b545cd8c432
Henri Heyden <henri@heyden5.de>
2024-01-19 16:19:31 +0100

    TODO: Add more comments and maybe use different syntax for cleaner code.

--- deklprog-project/src/SLD.hs
+++ deklprog-project/src/SLD.hs
@@ -1,6 +1,6 @@
 module SLD
   ( SLDTree (..),
-    -- sld,
+    sld,
     -- Strategy,
     -- dfs,
     -- bfs,
@@ -10,7 +10,26 @@ where
 
 import Base.Type
 import Subst
+import Unification
+import Vars
+import Rename
 
 -- Data type for an SLD tree
 data SLDTree = SLDTree Goal [(Subst, SLDTree)]
   deriving (Show)
+
+sld :: Prog -> Goal -> SLDTree
+sld (Prog rules) (Goal goals) = SLDTree (Goal goals) (build rules goals (allVars (Goal goals)))
+  where
+    build :: [Rule] -> [Term] -> [VarName] -> [(Subst, SLDTree)]
+    build _ [] bl = []
+    build [] _ bl = []
+    build (rule:rs) mg@(g:gs) bl =
+      let r@(Rule t ts) = rename bl rule in
+        let bl1 = bl ++ allVars r in
+          case unify t g of
+            Nothing -> build rs mg bl -- ignore ununified braches
+            Just s ->                 -- if unifiable
+              let ags = map (apply s) (ts ++ gs) in
+                (s, SLDTree (Goal ags) (build rules ags bl1)) -- 
+                  : build rs mg bl1 -- append rest
